5267c1e20bd173b8fa9963c33895e5b5d7a5f88a
Sergio Rodrigues <sergio@devindex.com.br>
2017-04-06 00:53:03 -0300

    Refactoring: Now `v-mask-model` is an alternative for `v-model` (components)

--- src/masks/model.js
+++ src/masks/model.js
@@ -1,15 +1,27 @@
-function getImputElement(el, vnode) {
-  return vnode.tag === 'input' ? el : el.querySelector('input');
-}
+import {getInputElement} from '../helpers';
 
 export default {
-  bind(el, {value}, vnode) {
-    el = getImputElement(el, vnode);
+  bind(el, {value, expression}, vnode) {
+    el = getInputElement(el, vnode);
+
+    const expressionParts = expression.replace(/\[(\d+)]/, '.$1').split('.');
+
+    const updateModelValue = val => {
+      let obj = vnode.context;
+      let parts = expressionParts.slice(0);
+      while (parts.length > 1) {
+        obj = obj[parts.shift()];
+      }
+      return obj[parts.shift()] = val;
+    };
+
+    el.addEventListener('input', ({target}) => updateModelValue(target.value), false);
+
     el.value = value;
   },
   update(el, {value, oldValue}, vnode) {
     if (value !== oldValue) {
-      el = getImputElement(el, vnode);
+      el = getInputElement(el, vnode);
       el.value = value;
     }
   }
