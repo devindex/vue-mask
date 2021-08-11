4630d5416efc8a007e12638d64cfefc7daa6fa43
Sergio Rodrigues Pinto <sergio@devindex.com.br>
2021-08-11 18:17:57 -0300

    [examples] Documentation layout improvements

--- examples/App.vue
+++ examples/App.vue
@@ -1,129 +1,167 @@
 <template>
-  <div>
-    <h1>Vue Mask</h1>
-
-    <div class="field">
-      <button @click="resetValues">Empty values</button>
-      <button @click="setValues">Default values</button>
+  <div class="container grid-sm doc-page">
+    <div class="columns pb-2">
+      <div class="column">
+        <h2 class="badge" data-badge="Vue 2">Vue Mask</h2>
+      </div>
+      <div class="column text-right">
+        <button class="btn btn-error mr-2" @click="clearValues">Clear values</button>
+        <button class="btn btn-primary" @click="setValues">Set values</button>
+      </div>
     </div>
 
-    <div class="field">
-      <label>Generic <span>[ {{ mask.generic }} ]</span></label>
-      <input type="text" name="generic" v-model="mask.generic" v-mask="'AAAA-AAAA-AAAA-AAAA'">
+    <div class="form-group">
+      <label class="form-label" for="generic">Mask</label>
+      <div class="input-group">
+        <input class="form-input" type="text" id="generic"
+               v-model="mask.generic" v-mask="'AAAA-AAAA-AAAA-AAAA'">
+        <span class="input-group-addon">{{ mask.generic }}</span>
+      </div>
+      <input-code directive="v-mask" value="'AAAA-AAAA-AAAA-AAAA'"></input-code>
     </div>
 
-    <div class="field">
-      <label>Data <span>[ {{ mask.date }} ]</span></label>
-      <input type="text" name="date" v-model="mask.date" v-mask-date.br>
+    <div class="form-group">
+      <label class="form-label" for="date">Date</label>
+      <div class="input-group">
+        <input class="form-input" type="text" id="date"
+               v-model="mask.date" v-mask-date.br>
+        <span class="input-group-addon">{{ mask.date }}</span>
+      </div>
+      <input-code directive="v-mask-date.br"></input-code>
     </div>
 
-    <div class="field">
-      <label>Decimal <span>[ {{ mask.decimal }} ]</span></label>
-      <input type="text" name="decimal" v-model="mask.decimal" v-mask-decimal.br="2">
+    <div class="form-group">
+      <label class="form-label" for="decimal">Decimal</label>
+      <div class="input-group">
+        <input class="form-input" type="text" id="decimal"
+               v-model="mask.decimal" v-mask-decimal.br="2">
+        <span class="input-group-addon">{{ mask.decimal }}</span>
+      </div>
+      <input-code directive="v-mask-decimal.br" value="2"></input-code>
     </div>
 
-    <div class="field">
-      <label>Number <span>[ {{ mask.number }} ]</span></label>
-      <input type="text" name="number" v-model="mask.number" v-mask-number>
+    <div class="form-group">
+      <label class="form-label" for="number">Number</label>
+      <div class="input-group">
+        <input class="form-input" type="text" id="number"
+               v-model="mask.number" v-mask-number>
+        <span class="input-group-addon">{{ mask.number }}</span>
+      </div>
+      <input-code directive="v-mask-number"></input-code>
     </div>
 
-    <div class="field">
-      <label>Phone <span>[ {{ mask.phone }} ]</span></label>
-      <input type="text" name="obj.phone" v-model="mask.phone" v-mask-phone.br>
+    <div class="form-group">
+      <label class="form-label" for="phone">Phone</label>
+      <div class="input-group">
+        <input class="form-input" type="text" id="phone"
+               v-model="mask.phone" v-mask-phone.br>
+        <span class="input-group-addon">{{ mask.phone }}</span>
+      </div>
+      <input-code directive="v-mask-phone.br"></input-code>
     </div>
 
-    <div class="field">
-      <label>CPF (BR)<span>[ {{ mask.cpf }} ]</span></label>
-      <input type="text" name="cpf" v-model="mask.cpf" v-mask-cpf>
+    <div class="form-group">
+      <label class="form-label" for="cpf">CPF (BR)</label>
+      <div class="input-group">
+        <input class="form-input" type="text" id="cpf"
+               v-model="mask.cpf" v-mask-cpf>
+        <span class="input-group-addon">{{ mask.cpf }}</span>
+      </div>
+      <input-code directive="v-mask-cpf"></input-code>
     </div>
 
-    <div class="field">
-      <label>CNPJ (BR)<span>[ {{ mask.cnpj }} ]</span></label>
-      <input type="text" name="cnpj" v-model="mask.cnpj" v-mask-cnpj>
+    <div class="form-group">
+      <label class="form-label" for="cnpj">CNPJ (BR)</label>
+      <div class="input-group">
+        <input class="form-input" type="text" id="cnpj"
+               v-model="mask.cnpj" v-mask-cnpj>
+        <span class="input-group-addon">{{ mask.cnpj }}</span>
+      </div>
+      <input-code directive="v-mask-cnpj"></input-code>
     </div>
 
-    <div class="field">
-      <label>CEP (BR)<span>[ {{ mask.cep }} ]</span></label>
-      <input type="text" name="cep" v-model="mask.cep" v-mask-cep>
+    <div class="form-group">
+      <label class="form-label" for="cep">CEP (BR)</label>
+      <div class="input-group">
+        <input class="form-input" type="text" id="cep"
+               v-model="mask.cep" v-mask-cep>
+        <span class="input-group-addon">{{ mask.cep }}</span>
+      </div>
+      <input-code directive="v-mask-cep"></input-code>
     </div>
 
-    <div class="field">
-      <label>Credit Card<span>[ {{ mask.cc }} ]</span></label>
-      <input type="text" name="cc" v-model="mask.cc" v-mask-cc>
+    <div class="form-group">
+      <label class="form-label" for="cc">Credit Card</label>
+      <div class="input-group">
+        <input class="form-input" type="text" id="cc"
+               v-model="mask.cc" v-mask-cc>
+        <span class="input-group-addon">{{ mask.cc }}</span>
+      </div>
+      <input-code directive="v-mask-cc"></input-code>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data () {
+import InputCode from './InputCode.vue';
+
+export default {
+  components: {
+    InputCode,
+  },
+  data () {
+    return {
+      mask: this.sampleValues(),
+    };
+  },
+  methods: {
+    clearValues() {
+      this.mask = this.emptyData();
+    },
+    setValues() {
+      this.mask = this.sampleValues();
+    },
+    sampleValues() {
       return {
-        mask: this.blankData(),
+        generic: '527407f42b4fa278',
+        date: '31/03/2017',
+        decimal: '987654321',
+        number: '9876543210',
+        phone: '41987654321',
+        cpf: '38558724903',
+        cnpj: '24168633000171',
+        cep: '80010170',
+        cc: '4539416887805240'
       };
     },
-    mounted () {
-      this.setValues();
+    emptyData() {
+      return {
+        generic: '',
+        date: '',
+        decimal: '',
+        number: '',
+        phone: '',
+        cpf: '',
+        cnpj: '',
+        cep: '',
+        cc: '',
+      }
     },
-    methods: {
-      resetValues() {
-        this.mask = this.blankData();
-      },
-      setValues () {
-        this.mask.generic = '527407f42b4fa278';
-        this.mask.date = '31/03/2017';
-        this.mask.decimal = 987654321;
-        this.mask.number = 9876543210;
-        this.mask.phone = '41987654321';
-        this.mask.cpf = '38558724903';
-        this.mask.cnpj = '24168633000171';
-        this.mask.cep = '80010170';
-        this.mask.cc = '4539416887805240';
-      },
-      blankData() {
-        return {
-          generic: '',
-          date: '',
-          decimal: '',
-          number: '',
-          phone: '',
-          cpf: '',
-          cnpj: '',
-          cep: '',
-          cc: ''
-        };
-      },
-    }
-  }
+  },
+}
 </script>
 
 <style>
-  body, input {
-    font-family: sans-serif;
-  }
-
-  .field {
-    margin-bottom: 20px;
-    color: #444;
-  }
-
-  .field label {
-    display: inline-block;
-    margin-bottom: 5px;
-    font-size: 14px;
-  }
-
-  .field input {
-    display: block;
-    width: 300px;
-    padding: 8px;
-    font-size: 14px;
-    border: 1px solid #ddd;
-    border-radius: 3px;
-  }
-
-  .field span {
-    margin-left: 10px;
-    font-size: 13px;
-    color: rgba(0, 0, 0, .7);
-  }
+.doc-page {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+}
+.doc-page .badge[data-badge]::after {
+  margin-left: 0.4rem;
+}
+.doc-page .input-group-addon {
+  color: #aaa;
+  min-width: 35%;
+  text-align: right;
+}
 </style>
---
+++ examples/InputCode.vue
@@ -0,0 +1,35 @@
+<template>
+  <pre class="input-code"><code v-text="inputSrc"></code></pre>
+</template>
+
+<script>
+export default {
+  props: {
+    directive: {
+      type: String,
+      default: 'v-mask'
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+  computed: {
+    inputSrc() {
+      const directive = this.directive.replace('v-', '');
+      const value = this.value ? `="${this.value}"` : '';
+      return `<input type="text" v-model="value" v-${directive}${value}>`;
+    },
+  },
+}
+</script>
+
+<style>
+.input-code {
+  margin: 0.2rem 0 0 0;
+}
+.input-code code {
+  font-size: 0.6rem;
+  padding: 0.2rem 0.4rem;
+}
+</style>
