85b473a0a31ea2ea49cf51e16063b27cb1156b20
Sergio Rodrigues Pinto <sergio@devindex.com.br>
2021-08-14 16:27:49 -0300

    [directive] Bug fix: mask was not being applied in some situations

--- src/directive.js
+++ src/directive.js
@@ -2,67 +2,85 @@ import { getInputElement, createEvent } from './helpers';
 
 function updater(el, masker) {
   const currentValue = el.value;
+  const oldValue = el.dataset.value;
+
+  if (oldValue === currentValue) {
+    return;
+  }
+
   const newValue = masker(currentValue, { el });
 
-  if (newValue !== currentValue) {
-    // Get current cursor position
-    let position = el.selectionEnd;
+  if (newValue === currentValue) {
+    el.dataset.value = currentValue;
+    return;
+  }
 
-    // Find next cursor position
-    if (position === currentValue.length) {
-      position = newValue.length;
-    } else if (position > 0 && position <= newValue.length) {
-      const digit = currentValue.charAt(position - 1);
-      // while(
-      //   position < newValue.length
-      //   && newValue.charAt(position - 1) !== digit
-      //   ) {
-      //   position++;
-      // }
-      if (digit !== newValue.charAt(position - 1)) {
-        if (digit === newValue.charAt(position)) {
-          position += 1;
-        } else if (digit === newValue.charAt(position - 2)) {
-          position -= 1;
-        }
+  // Get current cursor position
+  let position = el.selectionEnd;
+
+  // Find next cursor position
+  if (position === currentValue.length) {
+    position = newValue.length;
+  } else if (position > 0 && position <= newValue.length) {
+    const digit = currentValue.charAt(position - 1);
+
+    if (digit !== newValue.charAt(position - 1)) {
+      if (digit === newValue.charAt(position)) {
+        position += 1;
+      } else if (digit === newValue.charAt(position - 2)) {
+        position -= 1;
       }
     }
-
-    el.value = newValue;
-
-    if (el === document.activeElement) {
-      // Restore cursor position
-      el.setSelectionRange(position, position);
-    }
-
-    el.dispatchEvent(createEvent('input'));
   }
+
+  el.value = newValue;
+  el.dataset.value = newValue;
+
+  if (el === document.activeElement) {
+    // Restore cursor position
+    el.setSelectionRange(position, position);
+  }
+
+  el.dispatchEvent(createEvent('input'));
 }
 
 export default function make(maskerFn) {
-  let masker;
-  let inputEl;
+  const maskerMap = new WeakMap();
+  const inputMap = new WeakMap();
+  // const eventMap = new WeakMap();
 
   return {
     beforeMount(el, binding) {
-      masker = maskerFn({
+      const masker = maskerFn({
         value: binding.value,
         locale: binding.arg || Object.keys(binding.modifiers)[0] || null,
       });
 
-      inputEl = getInputElement(el);
+      const inputEl = getInputElement(el);
 
-      // inputEl.oninput = ({ isTrusted, inputType = null }) => {
+      // const eventHandler = ({ isTrusted }) => {
       //   if (isTrusted) {
-      //     updater(el, masker);
+      //     updater(inputEl, masker);
       //   }
       // };
+
+      maskerMap.set(el, masker);
+      inputMap.set(el, inputEl);
+      // eventMap.set(el, eventHandler);
+
+      // inputEl.addEventListener('input', eventHandler);
     },
-    mounted() {
-      updater(inputEl, masker);
+    mounted(el) {
+      updater(inputMap.get(el), maskerMap.get(el));
     },
-    updated() {
-      updater(inputEl, masker);
+    updated(el) {
+      updater(inputMap.get(el), maskerMap.get(el));
     },
+    unmounted(el) {
+      // el.removeEventListener('input', inputMap.get(el));
+      maskerMap.delete(el);
+      inputMap.delete(el);
+      // eventMap.delete(el);
+    }
   }
 }
